d937682d72a97fe480e3fdadbbcd045238ee5545
smlnx <alexander.is.dev@gmail.com>
2020-09-29 14:28:13 +0300

    fix(/src/components/app.component.jsx) eslint errors

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -1,13 +1,8 @@
-import React, { Component } from "react";
-// import s from "./app.component.css";
-class App extends Component {
-  render() {
-    return (
-      <div className='wrapper'>
-        <h1>Hello React Webpack</h1>
-        <img src='/assets/img/homer-u-hoo.png'/>
-      </div>
-    );
-  }
-}
-export default App;
+import React from 'react';
+
+export default () => (
+  <div className="wrapper">
+    <h1>Hello React Webpack</h1>
+    <img src="/assets/img/homer-u-hoo.png" alt="" />
+  </div>
+);
